f57a4b733201346d0dbf2b311406062437e073ea
Robert Maynard <rmaynard@nvidia.com>
2021-05-20 14:17:06 -0400

    Verify that rapids-cmake always preserves CPM arguments
    Per https://github.com/rapidsai/rapids-cmake/issues/16
    We need to make sure that we properly escape CPM arguments so
    that `OPTIONAL` arguments are passed correctly.
    
    This adds a collection of tests to verify that we always
    generate a CPMFindPackage call with all arguments in
    quotes

--- testing/cpm/cpm_find-existing-build-dir/RapidsTestFindConfig.cmake
+++ testing/cpm/cpm_find-existing-build-dir/RapidsTestFindConfig.cmake
@@ -13,6 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #=============================================================================
+@PACKAGE_INIT@
 
-add_cmake_config_test( cpm_find-existing-build-dir )
-add_cmake_config_test( cpm_find-options-escaped )
+include("${CMAKE_CURRENT_LIST_DIR}/RapidsTestFindConfigVersion.cmake")
+
+add_library(RapidsTest::RapidsTest IMPORTED INTERFACE GLOBAL)
+
+check_required_components(RapidsTest2)
