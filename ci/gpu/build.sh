d25cbcf2209433f3c87faf2d6f45fe9c8014e9aa
Dillon Cullinan <dcullinan92@gmail.com>
2021-07-19 11:17:16 -0400

    ENH Replace gpuci_conda_retry with gpuci_mamba_retry

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -42,7 +42,7 @@ gpuci_logger "Activate conda env"
 conda activate rapids
 
 gpuci_logger "Installing packages needed for rapids-cmake"
-gpuci_conda_retry  install -y \
+gpuci_mamba_retry  install -y \
                   "cudatoolkit=$CUDA_REL" \
                   "rapids-build-env=$MINOR_VERSION.*"
 
