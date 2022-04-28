758d5c4128fceaf9132bf9924177b3e37ec15835
Vyas Ramasubramani <vyasr@nvidia.com>
2022-04-28 13:55:58 -0700

    Update argument parsing.

--- rapids-cmake/cython/create_modules.cmake
+++ rapids-cmake/cython/create_modules.cmake
@@ -27,36 +27,41 @@ Generate C(++) from Cython and create Python modules.
 Creates a Cython target for a module, then adds a corresponding Python
 extension module. This function must be called after rapids_cython_init.
 
-``cython_modules``
+``MODULES``
   The list of modules to build.
 
-``linked_libraries``
+``LINKED_LIBRARIES``
   The list of libraries that need to be linked into all modules. In RAPIDS,
   this list usually contains (at minimum) the corresponding C++ libraries.
 
-``install_base_directory``
+``INSTALL_ROOT``
   The source directory of the project. This directory is used to compute the
   relative install path, which is necessary to propertly support differently
   configured installations such as installing in place vs. out of place.
 
 #]=======================================================================]
-function(rapids_cython_create_modules cython_modules linked_libraries install_base_directory)
+function(rapids_cython_create_modules)
   rapids_cython_verify_init()
 
-  foreach(cython_module ${cython_modules})
+  set(_rapids_cython_options)
+  set(_rapids_cython_one_value INSTALL_BASE_DIRECTORY)
+  set(_rapids_cython_multi_value MODULES LINKED_LIBRARIES)
+  cmake_parse_arguments(RAPIDS_CYTHON "${_rapids_cython_options}" "${_rapids_cython_one_value}" "${_rapids_cython_multi_value}" ${ARGN})
+
+  foreach(cython_module ${RAPIDS_CYTHON_MODULES})
     add_cython_target(${cython_module} CXX PY3)
     add_library(${cython_module} MODULE ${cython_module})
     python_extension_module(${cython_module})
 
     # To avoid libraries being prefixed with "lib".
     set_target_properties(${cython_module} PROPERTIES PREFIX "")
-    foreach(lib ${linked_libraries})
+    foreach(lib ${RAPIDS_CYTHON_LINKED_LIBRARIES})
       target_link_libraries(${cython_module} PUBLIC ${lib})
     endforeach()
 
     # Compute the install directory relative to the source and rely on installs being relative to
     # the CMAKE_PREFIX_PATH for e.g. editable installs.
-    cmake_path(RELATIVE_PATH CMAKE_CURRENT_SOURCE_DIR BASE_DIRECTORY ${install_base_directory}
+    cmake_path(RELATIVE_PATH CMAKE_CURRENT_SOURCE_DIR BASE_DIRECTORY ${RAPIDS_CYTHON_INSTALL_ROOT}
                OUTPUT_VARIABLE install_dst)
     install(TARGETS ${cython_module} DESTINATION ${install_dst})
   endforeach(cython_module ${cython_sources})
