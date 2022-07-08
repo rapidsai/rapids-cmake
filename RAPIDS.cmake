562bde9464e49d87064866838f4ed5662ebaa677
Vyas Ramasubramani <vyasr@nvidia.com>
2022-07-08 13:45:35 -0700

    Revert "Change fetch for testing."
    This reverts commit f9227c765c9a547a33e5988423b3071459486c80.

--- RAPIDS.cmake
+++ RAPIDS.cmake
@@ -21,8 +21,8 @@ set(rapids-cmake-version 22.08)
 include(FetchContent)
 FetchContent_Declare(
   rapids-cmake
-  GIT_REPOSITORY https://github.com/vyasr/rapids-cmake.git
-  GIT_TAG        reupdate_nvcomp
+  GIT_REPOSITORY https://github.com/rapidsai/rapids-cmake.git
+  GIT_TAG        branch-${rapids-cmake-version}
 )
 FetchContent_GetProperties(rapids-cmake)
 if(rapids-cmake_POPULATED)
