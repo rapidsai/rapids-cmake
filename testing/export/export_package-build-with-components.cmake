b517a73f8f0f3fcd2e09b96e9f226e0560b98e89
Robert Maynard <rmaynard@nvidia.com>
2022-08-01 13:39:13 -0400

    rapids_export_package understands components

--- testing/export/export_package-build-with-components.cmake
+++ testing/export/export_package-build-with-components.cmake
@@ -15,7 +15,7 @@
 #=============================================================================
 include(${rapids-cmake-dir}/export/package.cmake)
 
-rapids_export_package( build FAKE_PACKAGE test_export_set COMPONENTS 14.7.2)
+rapids_export_package( build FAKE_PACKAGE test_export_set COMPONENTS comp1 comp2 comp3)
 
 # Verify that package configuration files exist
 set(path "${CMAKE_BINARY_DIR}/rapids-cmake/test_export_set/build/package_FAKE_PACKAGE.cmake")
@@ -23,10 +23,11 @@ if(NOT EXISTS "${path}")
   message(FATAL_ERROR "rapids_export_package failed to generate a find_package configuration")
 endif()
 
-# verify that the expected COMPONENTS exists in FAKE_PACKAGE.cmake
-set(to_match_string [=[COMPONENTS 14.7.2)]=])
+# verify that the expected components is in FAKE_PACKAGE.cmake
+set(to_match_string [=[COMPONENTS comp1;comp2;comp3)]=])
 file(READ "${path}" contents)
 string(FIND "${contents}" "${to_match_string}" is_found)
 if(is_found EQUAL -1)
   message(FATAL_ERROR "rapids_export_package failed to generate a find_package configuration with COMPONENTS")
 endif()
+
--- testing/export/export_package-build-with-empty-components.cmake
+++ testing/export/export_package-build-with-empty-components.cmake
@@ -15,7 +15,7 @@
 #=============================================================================
 include(${rapids-cmake-dir}/export/package.cmake)
 
-rapids_export_package( build FAKE_PACKAGE test_export_set COMPONENTS 14.7.2)
+rapids_export_package( build FAKE_PACKAGE test_export_set COMPONENTS)
 
 # Verify that package configuration files exist
 set(path "${CMAKE_BINARY_DIR}/rapids-cmake/test_export_set/build/package_FAKE_PACKAGE.cmake")
@@ -23,10 +23,11 @@ if(NOT EXISTS "${path}")
   message(FATAL_ERROR "rapids_export_package failed to generate a find_package configuration")
 endif()
 
-# verify that the expected COMPONENTS exists in FAKE_PACKAGE.cmake
-set(to_match_string [=[COMPONENTS 14.7.2)]=])
+# verify that `COMPONENTS` isn't in FAKE_PACKAGE.cmake
+set(to_match_string [=[COMPONENTS)]=])
 file(READ "${path}" contents)
 string(FIND "${contents}" "${to_match_string}" is_found)
-if(is_found EQUAL -1)
-  message(FATAL_ERROR "rapids_export_package failed to generate a find_package configuration with COMPONENTS")
+if(NOT is_found EQUAL -1)
+  message(FATAL_ERROR "rapids_export_package generated a find_package configuration with COMPONENTS")
 endif()
+
