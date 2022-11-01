4e8f8d251abc50add80ccf55f3ce9ab0555506bf
Vyas Ramasubramani <vyasr@nvidia.com>
2022-11-01 09:47:33 -0700

    Add ability to specify library directories for target rpaths (#295)
    Python extension modules built using scikit-build and rapids-cmake are typically designed around a model where a C++ library is built and the resulting Cython extension modules must link to it. While it is in principle possible to use both scikit-build and rapids-cmake with only Cython, that is not the case for any of the major RAPIDS packages. When the C++ component is built via scikit-build and installed into the final package, it must also be discoverable by the extension modules. Currently this is accomplished by propagating a variable `RAPIDS_CYTHON_CREATED_MODULES` from `rapids_cython_create_modules` that calling functions can use to get the list of targets whose RPATHs need updating, and setting these RPATHs is the responsibility of the caller. However, this approach is error-prone and frequently results in incorrect RPATH specifications.
    
    This PR automates the process by having the calling code provide a set of directories where compiled libraries may be placed. The RPATHs of all extension modules built via `rapids_cython_create_modules` are then automatically updated to include those library directories.
    
    Authors:
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    Approvers:
      - Robert Maynard (https://github.com/robertmaynard)
    
    URL: https://github.com/rapidsai/rapids-cmake/pull/295

--- rapids-cmake/cython/create_modules.cmake
+++ rapids-cmake/cython/create_modules.cmake
@@ -57,6 +57,14 @@ $ORIGIN.
   useful when multiple Cython modules in different subpackages of the the same
   project have the same name. The default prefix is the empty string.
 
+``ASSOCIATED_TARGETS``
+  A list of targets that are associated with the Cython targets created in this
+  function. The target<-->associated target mapping is stored and may be
+  leveraged by the following functions:
+  - :code:command:`rapids_cython_add_rpath_entries` accepts a path for an
+    associated target and updates the RPATH of each target with which that
+    associated target is associated.
+
 Result Variables
 ^^^^^^^^^^^^^^^^
   :cmake:variable:`RAPIDS_CYTHON_CREATED_TARGETS` will be set to a list of
@@ -71,7 +79,7 @@ function(rapids_cython_create_modules)
 
   set(_rapids_cython_options CXX)
   set(_rapids_cython_one_value INSTALL_DIR MODULE_PREFIX)
-  set(_rapids_cython_multi_value SOURCE_FILES LINKED_LIBRARIES)
+  set(_rapids_cython_multi_value SOURCE_FILES LINKED_LIBRARIES ASSOCIATED_TARGETS)
   cmake_parse_arguments(_RAPIDS_CYTHON "${_rapids_cython_options}" "${_rapids_cython_one_value}"
                         "${_rapids_cython_multi_value}" ${ARGN})
 
@@ -119,6 +127,12 @@ function(rapids_cython_create_modules)
 
     # Default the INSTALL_RPATH for all modules to $ORIGIN.
     set_target_properties(${cython_module} PROPERTIES INSTALL_RPATH "\$ORIGIN")
+
+    # Store any provided associated targets in a global list
+    foreach(associated_target IN LISTS _RAPIDS_CYTHON_ASSOCIATED_TARGETS)
+      set_property(GLOBAL PROPERTY "rapids_cython_associations_${associated_target}"
+                                   "${cython_module}" APPEND)
+    endforeach()
 
     list(APPEND CREATED_TARGETS "${cython_module}")
   endforeach()
