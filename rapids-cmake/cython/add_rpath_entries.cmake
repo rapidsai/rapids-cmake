5978af4ba8e4c7ab1049b9ad25faf6676ce65830
Jake Awe <jawe@nvidia.com>
2023-02-14 12:03:49 -0600

    fix checks

--- rapids-cmake/cython/add_rpath_entries.cmake
+++ rapids-cmake/cython/add_rpath_entries.cmake
@@ -1,5 +1,5 @@
 # =============================================================================
-# Copyright (c) 2022, NVIDIA CORPORATION.
+# Copyright (c) 2022-2023, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
 # in compliance with the License. You may obtain a copy of the License at
@@ -15,7 +15,6 @@
 include_guard(GLOBAL)
 
 #[=======================================================================[.rst:
-
 rapids_cython_add_rpath_entries
 -------------------------------
 .. versionadded:: v22.12.00
@@ -50,7 +49,6 @@ will have no effect on targets created afterwards.
   `rapids_cython_add_rpath_entries` is called.
 
 #]=======================================================================]
-
 function(rapids_cython_add_rpath_entries)
   list(APPEND CMAKE_MESSAGE_CONTEXT "rapids.cython.add_rpath_entries")
 
