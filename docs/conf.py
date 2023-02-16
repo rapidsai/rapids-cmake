76ce4ed75940c9ccdfc19d3fbd977a2a495dfae6
Jake Awe <jawe@nvidia.com>
2023-02-16 14:59:21 -0600

    rm os import

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,6 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import sphinx_rtd_theme
-import os
 
 # -- Project information -----------------------------------------------------
 
