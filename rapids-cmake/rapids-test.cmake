2781c11d28c2b47d718f3c6860ab240b67b95b67
Robert Maynard <rmaynard@nvidia.com>
2023-02-28 12:51:47 -0500

    rapids-cmake/rapids-test.cmake includes generate_resource_spec

--- rapids-cmake/rapids-test.cmake
+++ rapids-cmake/rapids-test.cmake
@@ -17,5 +17,6 @@ include_guard(GLOBAL)
 
 include(${CMAKE_CURRENT_LIST_DIR}/test/init.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/test/add.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/test/generate_resource_spec.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/test/gpu_requirements.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/test/install_relocatable.cmake)
