8b246a2f3fe23529dc6036ca836c8d53b06b55c5
Vyas Ramasubramani <vyasr@nvidia.com>
2023-03-30 08:01:45 -0700

    Revert "Define Cython language_level explicitly. (#394)"
    This reverts commit 0354669d2e63c4efe53812ae91e4927cc8e1a01a.

--- rapids-cmake/cython/init.cmake
+++ rapids-cmake/cython/init.cmake
@@ -1,5 +1,5 @@
 # =============================================================================
-# Copyright (c) 2022-2023, NVIDIA CORPORATION.
+# Copyright (c) 2022, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
 # in compliance with the License. You may obtain a copy of the License at
@@ -57,9 +57,7 @@ macro(rapids_cython_init)
     include("${rapids-cmake-dir}/cython/detail/skbuild_patches.cmake")
 
     if(NOT CYTHON_FLAGS)
-      set(CYTHON_FLAGS
-          "--directive always_allow_keywords=True,binding=True,embedsignature=True,language_level=3str"
-      )
+      set(CYTHON_FLAGS "--directive binding=True,embedsignature=True,always_allow_keywords=True")
     endif()
 
     # Flag
