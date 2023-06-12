b37a289c7a6d2d338ec669c60d4a04a3538fdb4c
Robert Maynard <rmaynard@nvidia.com>
2023-06-12 14:43:08 -0400

    support_conda_env support host and build CTK 12 locations
    For cuda 12+ the conda layout has CTK components in both host
    and build. Update the CMAKE_PREFIX search locations and rpath-link
    entries for this new layout.

--- rapids-cmake/cmake/support_conda_env.cmake
+++ rapids-cmake/cmake/support_conda_env.cmake
@@ -31,12 +31,20 @@ Creates a global interface target called `target_name` that holds
 the CONDA include and link directories, when executed.
 
 Also offers the ability to modify :cmake:variable:`CMAKE_PREFIX_PATH <cmake:variable:CMAKE_PREFIX_PATH>` to
-include the paths in environment variables `PREFIX`, `BUILD_PREFIX`,
-and `CONDA_PREFIX` based on the current CONDA environment.
+include the following paths based on the current CONDA environment:
+
+  - `PREFIX`
+  - `BUILD_PREFIX`
+  - `CONDA_PREFIX`
+
+  .. versionadded:: v23.08.00
+
+    - `PREFIX`/targets/<cuda_target_platform>/
 
 ``MODIFY_PREFIX_PATH``
-    When in a conda build environment the contents of `$ENV{PREFIX}` and `$ENV{BUILD_PREFIX}`
-    will be inserted to the front of :cmake:variable:`CMAKE_PREFIX_PATH <cmake:variable:CMAKE_PREFIX_PATH>`.
+    When in a conda build environment the contents of `$ENV{PREFIX}`,
+    `$ENV{PREFIX}`/targets/<cuda_target_platform>/`, and `$ENV{BUILD_PREFIX}` will be inserted to the
+    front of :cmake:variable:`CMAKE_PREFIX_PATH <cmake:variable:CMAKE_PREFIX_PATH>`.
 
     When in a conda environment the contents of `$ENV{CONDA_PREFIX}` will be inserted to
     the front of :cmake:variable:`CMAKE_PREFIX_PATH <cmake:variable:CMAKE_PREFIX_PATH>`.
@@ -51,6 +59,7 @@ Result Targets
   `target_name` target will be created only if called from a conda environment.
 
 #]=======================================================================]
+# cmake-lint: disable=R0912,R0915
 function(rapids_cmake_support_conda_env target)
   list(APPEND CMAKE_MESSAGE_CONTEXT "rapids.cmake.support_conda_env")
 
@@ -75,12 +84,31 @@ function(rapids_cmake_support_conda_env target)
     set(prefix_paths)
 
     if(in_conda_build)
+      # For conda-build we add the host requirements prefix to the cmake search paths So that raw
+      # `find_file` or `find_library` calls will find cuda components in the host component
+      set(target_platform $ENV{cross_target_platform}) # when target != cross_target
+      if(NOT target_platform)
+        set(target_platform $ENV{target_platform})
+      endif()
+      if("${target_platform}" STREQUAL "linux-64")
+        set(targetsDir "targets/x86_64-linux")
+      elseif("${target_platform}" STREQUAL "linux-ppc64le")
+        set(targetsDir "targets/ppc64le-linux")
+      elseif("${target_platform}" STREQUAL "linux-aarch64")
+        set(targetsDir "targets/sbsa-linux")
+      endif()
+
       target_include_directories(${target} INTERFACE "$ENV{PREFIX}/include"
                                                      "$ENV{BUILD_PREFIX}/include")
       target_link_directories(${target} INTERFACE "$ENV{PREFIX}/lib" "$ENV{BUILD_PREFIX}/lib")
 
       if(DEFINED CMAKE_SHARED_LIBRARY_RPATH_LINK_CUDA_FLAG
          OR DEFINED CMAKE_SHARED_LIBRARY_RPATH_LINK_CXX_FLAG)
+        if(DEFINED targetsDir)
+          target_link_options(${target} INTERFACE
+                              "$<HOST_LINK:SHELL:LINKER:-rpath-link=$ENV{PREFIX}/${targetsDir}/lib>"
+          )
+        endif()
         target_link_options(${target} INTERFACE
                             "$<HOST_LINK:SHELL:LINKER:-rpath-link=$ENV{PREFIX}/lib>")
         target_link_options(${target} INTERFACE
@@ -89,6 +117,9 @@ function(rapids_cmake_support_conda_env target)
 
       if(modify_prefix_path)
         list(PREPEND CMAKE_PREFIX_PATH "$ENV{PREFIX}" "$ENV{BUILD_PREFIX}")
+        if(DEFINED targetsDir)
+          list(PREPEND CMAKE_PREFIX_PATH "$ENV{PREFIX}/${targetsDir}")
+        endif()
         set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH}" PARENT_SCOPE)
         message(VERBOSE "Conda build detected, CMAKE_PREFIX_PATH set to: ${CMAKE_PREFIX_PATH}")
       endif()
