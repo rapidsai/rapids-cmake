a1a9831fd3bdf85c73a6725aab81d7c48ee16e85
Robert Maynard <rmaynard@nvidia.com>
2023-10-04 11:43:58 -0400

    Ensure deprecation tests produce error messages

--- testing/export/find_package_file-deprecated.cmake
+++ testing/export/find_package_file-deprecated.cmake
@@ -16,5 +16,5 @@
 include(${rapids-cmake-dir}/export/find_package_file.cmake)
 
 project(rapids-project LANGUAGES CUDA)
-
+set(CMAKE_ERROR_DEPRECATED ON)
 rapids_export_find_package_file(BUILD [=[${CMAKE_CURRENT_LIST_DIR}/fake/build/path]=] test_set)
