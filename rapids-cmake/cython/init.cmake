f38531d905f93cdd987b65c7fbe1cb526c0941a0
Vyas Ramasubramani <vyasr@nvidia.com>
2024-01-11 12:24:12 -0800

    Fix message context (#520)
    Resolves #514
    
    Authors:
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    Approvers:
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
    
    URL: https://github.com/rapidsai/rapids-cmake/pull/520

--- rapids-cmake/cython/init.cmake
+++ rapids-cmake/cython/init.cmake
@@ -1,5 +1,5 @@
 # =============================================================================
-# Copyright (c) 2022-2023, NVIDIA CORPORATION.
+# Copyright (c) 2022-2024, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
 # in compliance with the License. You may obtain a copy of the License at
@@ -68,4 +68,5 @@ macro(rapids_cython_init)
     # Flag
     set(RAPIDS_CYTHON_INITIALIZED TRUE)
   endif()
+  list(POP_BACK CMAKE_MESSAGE_CONTEXT)
 endmacro()
