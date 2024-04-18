40bf3c47e68bf0586cd8918dc0a43beb8ef00d3a
Robert Maynard <rmaynard@nvidia.com>
2024-04-18 16:09:32 -0400

    Update to cleaner code suggested in code review

--- rapids-cmake/test/detail/generate_installed_CTestTestfile.cmake
+++ rapids-cmake/test/detail/generate_installed_CTestTestfile.cmake
@@ -15,13 +15,7 @@
 #=============================================================================
 include_guard(GLOBAL)
 
-cmake_policy(SET CMP0007 NEW) # allow empty list entries
-cmake_policy(SET CMP0009 NEW) # don't follow symlinks
-cmake_policy(SET CMP0057 NEW) # allow `if( IN_LIST )`
-
-if(POLICY CMP0159)
-  cmake_policy(SET CMP0159 NEW)
-endif()
+cmake_minimum_required(VERSION 3.26...3.29)
 
 #[=[
 The goal of this script is to re-parse the `CTestTestfile`
