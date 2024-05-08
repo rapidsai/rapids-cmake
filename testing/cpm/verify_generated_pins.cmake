5206219abe0d8450415514edeee746c5475ae6bd
Robert Maynard <robertjmaynard@gmail.com>
2024-05-08 14:02:28 -0400

    Apply suggestions from code review
    Co-authored-by: Kyle Edwards <kyedwards@nvidia.com>

--- testing/cpm/verify_generated_pins.cmake
+++ testing/cpm/verify_generated_pins.cmake
@@ -18,8 +18,8 @@ function(verify_generated_pins target_name)
   set(_rapids_options)
   set(_rapids_one_value PIN_FILE)
   set(_rapids_multi_value PROJECTS PROJECTS_NOT_EXIST)
-  cmake_parse_arguments(_RAPIDS "${_rapids_options}" "${_rapids_one_value}"
-                        "${_rapids_multi_value}" ${ARGN})
+  cmake_parse_arguments(PARSE_ARGV 1 _RAPIDS "${_rapids_options}" "${_rapids_one_value}"
+                        "${_rapids_multi_value}")
 
   if(NOT DEFINED _RAPIDS_PROJECTS)
     message(FATAL_ERROR "verify_generated_pins must be called with `PROJECTS` to verify")
@@ -36,7 +36,7 @@ function(verify_generated_pins target_name)
   endforeach()
 
   add_custom_target(${target_name} ALL
-    COMMAND ${CMAKE_COMMAND} -S="${CMAKE_CURRENT_FUNCTION_LIST_DIR}/verify_generated_pins/" -B"${CMAKE_BINARY_DIR}/${target_name}_verify_build"
+    COMMAND ${CMAKE_COMMAND} "-S${CMAKE_CURRENT_FUNCTION_LIST_DIR}/verify_generated_pins/" "-B${CMAKE_BINARY_DIR}/${target_name}_verify_build"
     "-Drapids-cmake-dir=${rapids-cmake-dir}"
     "-Dpinned_versions_file=${_RAPIDS_PIN_FILE}"
     "-Dprojects-to-verify=\"${projects-to-verify}\""
