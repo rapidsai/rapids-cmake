24d16295ca4936de26725ba56a0e1f73b85c7d32
Robert Maynard <rmaynard@nvidia.com>
2024-06-04 10:12:07 -0400

    Correct style issues found by CI

--- rapids-cmake/cuda/init_architectures.cmake
+++ rapids-cmake/cuda/init_architectures.cmake
@@ -1,5 +1,5 @@
 #=============================================================================
-# Copyright (c) 2021-2023, NVIDIA CORPORATION.
+# Copyright (c) 2021-2024, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -72,12 +72,10 @@ function(rapids_cuda_init_architectures project_name)
   # If `CMAKE_CUDA_ARCHITECTURES` is not defined, build for all supported architectures. If
   # `CMAKE_CUDA_ARCHITECTURES` is set to an empty string (""), build for only the current
   # architecture. If `CMAKE_CUDA_ARCHITECTURES` is specified by the user, use user setting.
-  if(CMAKE_CUDA_ARCHITECTURES STREQUAL "RAPIDS" OR
-     CMAKE_CUDA_ARCHITECTURES STREQUAL "NATIVE")
+  if(CMAKE_CUDA_ARCHITECTURES STREQUAL "RAPIDS" OR CMAKE_CUDA_ARCHITECTURES STREQUAL "NATIVE")
     set(cuda_arch_mode "${CMAKE_CUDA_ARCHITECTURES}")
-  elseif(DEFINED ENV{CUDAARCHS} AND
-        ("$ENV{CUDAARCHS}" STREQUAL "RAPIDS" OR
-         "$ENV{CUDAARCHS}" STREQUAL "NATIVE"))
+  elseif(DEFINED ENV{CUDAARCHS} AND ("$ENV{CUDAARCHS}" STREQUAL "RAPIDS" OR "$ENV{CUDAARCHS}"
+                                                                            STREQUAL "NATIVE"))
     set(cuda_arch_mode "$ENV{CUDAARCHS}")
   elseif(NOT (DEFINED ENV{CUDAARCHS} OR DEFINED CMAKE_CUDA_ARCHITECTURES))
     set(cuda_arch_mode "RAPIDS")
