e1e725a2e8e24f2fd5be7b4857e5f69f62e23359
Kyle Edwards <kyedwards@nvidia.com>
2024-07-22 16:35:36 -0400

    Add rapids_cpm_bs_thread_pool()
    Add a function to download and export bshoshany/thread-pool.

---
+++ testing/cpm/cpm_bs_thread_pool-simple.cmake
@@ -0,0 +1,34 @@
+#=============================================================================
+# Copyright (c) 2024, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#=============================================================================
+include(${rapids-cmake-dir}/cpm/init.cmake)
+include(${rapids-cmake-dir}/cpm/bs_thread_pool.cmake)
+
+rapids_cpm_init()
+
+if(TARGET thread_pool)
+  message(FATAL_ERROR "Expected thread_pool not to exist")
+endif()
+
+rapids_cpm_bs_thread_pool()
+
+if(NOT TARGET thread_pool)
+  message(FATAL_ERROR "Expected thread_pool target to exist")
+endif()
+if(NOT TARGET BS::thread_pool)
+  message(FATAL_ERROR "Expected BS::thread_pool target to exist")
+endif()
+
+rapids_cpm_bs_thread_pool()
