cb2f49bbc064abd1a9e40a080d43deb77ac782a1
Ray Douglass <ray@raydouglass.com>
2024-09-19 11:43:06 -0400

    DOC v24.12 Updates [skip ci]

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ author = "NVIDIA"
 # built documents.
 #
 # The short X.Y version.
-version = "24.10"
+version = "24.12"
 # The full version, including alpha/beta/rc tags.
-release = "24.10.00"
+release = "24.12.00"
 
 
 # -- General configuration ---------------------------------------------------
