1b4f436092c6352074645f01c445ffe68141fe94
Vyas Ramasubramani <vyasr@nvidia.com>
2024-11-27 18:17:54 +0000

    First pass at enabling CPM download by default.

--- rapids-cmake/export/cpm.cmake
+++ rapids-cmake/export/cpm.cmake
@@ -31,6 +31,7 @@ given export set
                       <ExportSet>
                       CPM_ARGS <standard cpm args>
                       [GLOBAL_TARGETS <targets...>]
+                      [DEFAULT_DOWNLOAD_OPTION <option_name>]
                       )
 
 
@@ -53,6 +54,9 @@ generated information will include a :cmake:command:`CPMFindPackage` call for <P
   Which targets from this package should be made global. This information
   will be propagated to any associated export set.
 
+``DEFAULT_DOWNLOAD_OPTION``
+  If set, expose an option <option_name> in the config to use CPMAddPackage instead of CPMFindPackage. The option will default to true.
+
   .. note::
     It is an anti-pattern to use this command with `INSTALL` as most CMake
     based projects should be installed, and :cmake:command:`rapids_export_package(INSTALL` used
@@ -67,7 +71,7 @@ function(rapids_export_cpm type name export_set)
   string(TOLOWER ${type} type)
 
   set(options "")
-  set(one_value)
+  set(one_value DEFAULT_DOWNLOAD_OPTION)
   set(multi_value GLOBAL_TARGETS CPM_ARGS)
   cmake_parse_arguments(_RAPIDS "${options}" "${one_value}" "${multi_value}" ${ARGN})
 
