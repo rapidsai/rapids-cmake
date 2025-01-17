af4fe439e45f3479e728a181604e6d9c2a1bf0d0
Bradley Dice <bdice@bradleydice.com>
2025-01-17 14:12:17 -0600

    Use CCCL v2.7.0 final tag. (#743)
    CCCL is currently pinned to `v2.7.0-rc2`. Now that a final tag for `v2.7.0` is available, we should use that instead.
    
    See diff: https://github.com/NVIDIA/cccl/compare/v2.7.0-rc2...v2.7.0
    
    Authors:
      - Bradley Dice (https://github.com/bdice)
    
    Approvers:
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
    
    URL: https://github.com/rapidsai/rapids-cmake/pull/743

--- testing/cpm/cpm_cccl-version-2-7.cmake
+++ testing/cpm/cpm_cccl-version-2-7.cmake
@@ -1,5 +1,5 @@
 #=============================================================================
-# Copyright (c) 2024, NVIDIA CORPORATION.
+# Copyright (c) 2024-2025, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -32,7 +32,7 @@ file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/override.json
       "version": "2.7.0",
       "git_shallow": false,
       "git_url": "https://github.com/NVIDIA/cccl.git",
-      "git_tag": "v2.7.0-rc2"
+      "git_tag": "v2.7.0"
     }
   }
 }
