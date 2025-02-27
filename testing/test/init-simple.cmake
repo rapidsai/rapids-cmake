06b78346fa5e13d20c3c2979353b7fb573b6168a
Kyle Edwards <kyedwards@nvidia.com>
2025-02-27 11:15:27 -0500

    Use CTest dynamic resource spec generation
    CMake has supported dynamic resource spec generation since 3.28.
    Since we now require 3.30, use this feature instead of implementing
    it ourselves.

--- testing/test/init-simple.cmake
+++ testing/test/init-simple.cmake
@@ -1,5 +1,5 @@
 #=============================================================================
-# Copyright (c) 2022-2023, NVIDIA CORPORATION.
+# Copyright (c) 2022-2025, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,10 +19,16 @@ enable_language(CUDA)
 
 rapids_test_init()
 
-if(NOT DEFINED CTEST_RESOURCE_SPEC_FILE)
-  message(FATAL_ERROR "CTEST_RESOURCE_SPEC_FILE should be set after calling rapids_test_init")
+if(NOT TEST generate_resource_spec)
+  message(FATAL_ERROR "generate_resource_spec test should exist after calling rapids_test_init")
 endif()
 
-if(NOT EXISTS "${CTEST_RESOURCE_SPEC_FILE}")
-  message(FATAL_ERROR "CTEST_RESOURCE_SPEC_FILE should exist on disk after calling rapids_test_init")
+get_test_property(generate_resource_spec FIXTURES_SETUP fixtures_setup)
+if(NOT fixtures_setup STREQUAL "resource_spec")
+  message(FATAL_ERROR "generate_resource_spec FIXTURES_SETUP property should be set to \"resource_spec\"")
+endif()
+
+get_test_property(generate_resource_spec GENERATED_RESOURCE_SPEC_FILE grsf)
+if(NOT grsf STREQUAL "${CMAKE_CURRENT_BINARY_DIR}/resource_spec.json")
+  message(FATAL_ERROR "generate_resource_spec FIXTURES_SETUP property should be set to \"${CMAKE_CURRENT_BINARY_DIR}/resource_spec.json\"")
 endif()
