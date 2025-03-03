ca4b05475f5c6c22d7023f4a2c56f73c70f93d6b
Robert Maynard <rmaynard@nvidia.com>
2025-03-03 09:30:09 -0500

    Rollback to CPM 0.40.0 (#789)
    This is required to allow overrides to work with projects that have a custom source subdir value.
    
    See https://github.com/rapidsai/rapids-cmake/issues/788 for more information.
    
    Authors:
      - Robert Maynard (https://github.com/robertmaynard)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/rapids-cmake/pull/789

--- rapids-cmake/cpm/detail/download.cmake
+++ rapids-cmake/cpm/detail/download.cmake
@@ -52,8 +52,8 @@ function(rapids_cpm_download)
   list(APPEND CMAKE_MESSAGE_CONTEXT "rapids.cpm.download")
 
   # When changing version verify no new variables needs to be propagated
-  set(CPM_DOWNLOAD_VERSION 0.40.2)
-  set(CPM_DOWNLOAD_MD5_HASH 4d51aa9dab6104fad39c5b7a692d5e1c)
+  set(CPM_DOWNLOAD_VERSION 0.40.0)
+  set(CPM_DOWNLOAD_MD5_HASH 6c9866a0aa0f804a36fe8c3866fb8a2c)
 
   if(NOT DEFINED CPM_DOWNLOAD_LOCATION)
     if(CPM_SOURCE_CACHE)
