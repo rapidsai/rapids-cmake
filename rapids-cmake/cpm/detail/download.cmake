47ed0c6d52d84a55f9afb45ee4b6310376605068
Peter Andreas Entschev <peter@entschev.com>
2025-04-03 13:56:46 -0700

    Update CPM.cmake hash

--- rapids-cmake/cpm/detail/download.cmake
+++ rapids-cmake/cpm/detail/download.cmake
@@ -53,7 +53,7 @@ function(rapids_cpm_download)
 
   # When changing version verify no new variables needs to be propagated
   set(CPM_DOWNLOAD_VERSION 0.40.0)
-  set(CPM_DOWNLOAD_MD5_HASH 6c9866a0aa0f804a36fe8c3866fb8a2c)
+  set(CPM_DOWNLOAD_SHA256_HASH 7b354f3a5976c4626c876850c93944e52c83ec59a159ae5de5be7983f0e17a2a)
 
   if(NOT DEFINED CPM_DOWNLOAD_LOCATION)
     if(CPM_SOURCE_CACHE)
@@ -86,7 +86,7 @@ function(rapids_cpm_download)
     include("${rapids-cmake-dir}/cmake/download_with_retry.cmake")
     rapids_download_with_retry(https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake
                                ${CPM_DOWNLOAD_LOCATION}
-                               ${CPM_DOWNLOAD_MD5_HASH})
+                               ${CPM_DOWNLOAD_SHA256_HASH})
   endif()
 
   include(${CPM_DOWNLOAD_LOCATION})
