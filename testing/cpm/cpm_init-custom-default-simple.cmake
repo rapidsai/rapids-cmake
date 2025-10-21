39ced7438ffe66ca4f3e829d53310c36d26aa720
Kyle Edwards <kyedwards@nvidia.com>
2025-10-21 14:25:53 -0400

    Use SPDX for all copyright headers
    Issue: https://github.com/rapidsai/build-infra/issues/297

--- testing/cpm/cpm_init-custom-default-simple.cmake
+++ testing/cpm/cpm_init-custom-default-simple.cmake
@@ -1,5 +1,6 @@
 #=============================================================================
-# Copyright (c) 2024-2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2024-2025, NVIDIA CORPORATION.
+# SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
