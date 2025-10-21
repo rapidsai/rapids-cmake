3357d30f10a82b25b401bcdb681cf02312e6760f
Kyle Edwards <kyedwards@nvidia.com>
2025-10-21 15:34:14 -0400

    Use SPDX for all copyright headers (#933)
    Issue: https://github.com/rapidsai/build-infra/issues/297
    
    Authors:
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
    
    Approvers:
      - Robert Maynard (https://github.com/robertmaynard)
    
    URL: https://github.com/rapidsai/rapids-cmake/pull/933

--- rapids-cmake/cmake/download_with_retry.cmake
+++ rapids-cmake/cmake/download_with_retry.cmake
@@ -1,5 +1,5 @@
 #=============================================================================
-# Copyright (c) 2024-2025, NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: Copyright (c) 2024-2025, NVIDIA CORPORATION & AFFILIATES.
 # SPDX-License-Identifier: Apache-2.0
 #=============================================================================
 include_guard(GLOBAL)
