71a0083fcf6c88a56a1c3bc6ea8e15e9970bcb69
Robert Maynard <rmaynard@nvidia.com>
2026-01-28 17:28:50 -0500

    Update rapids-cmake to have a set of examples showing common patterns

---
+++ examples/export-feature/lib.cu
@@ -0,0 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <cstdio>
+
+void export_example() { printf("export-feature example\n"); }
