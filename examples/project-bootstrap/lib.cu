45f50b29e7a659bf9abb0ab11a070282b4f11071
Robert Maynard <rmaynard@nvidia.com>
2026-02-03 16:49:34 -0500

    Update to add even more examples

---
+++ examples/project-bootstrap/lib.cu
@@ -0,0 +1,8 @@
+// =============================================================================
+// SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION.
+// SPDX-License-Identifier: Apache-2.0
+// =============================================================================
+
+#include <cstdio>
+
+void project_bootstrap_function() { printf("project-bootstrap example library\n"); }
