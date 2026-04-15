0d3cdd25a427e8922a483d3feeb5f9c225011fa8
Robert Maynard <rmaynard@nvidia.com>
2026-04-15 12:57:54 -0400

    export_cpm: Improve heursitic to avoid saving CMake Package Redirector dir

--- testing/cpm/cpm_cccl-package-redirect.cmake
+++ testing/cpm/cpm_cccl-package-redirect.cmake
@@ -5,24 +5,26 @@
 # cmake-format: on
 # =============================================================================
 include(${rapids-cmake-dir}/cpm/init.cmake)
-include(${rapids-cmake-dir}/cpm/rmm.cmake)
+include(${rapids-cmake-dir}/cpm/cccl.cmake)
 
 rapids_cpm_init()
 
-rapids_cpm_rmm(INSTALL_EXPORT_SET example-exports)
+# First time without export set so that rapids_export_cpm doesn't record a location
+rapids_cpm_cccl()
 
 # Call find_package which will trigger the FindPackageRedirect logic
-find_package(rmm REQUIRED HINTS "${rmm_BINARY_DIR}")
+find_package(CCCL ${CCCL_VERSION} CONFIG REQUIRED)
 
-rapids_cpm_rmm(BUILD_EXPORT_SET example-exports INSTALL_EXPORT_SET example-exports)
+# Second time with an export set so that rapids_export_cpm now records a location
+rapids_cpm_cccl(BUILD_EXPORT_SET example-exports INSTALL_EXPORT_SET example-exports)
 
-# verify that the expected path exists in cpm_rmm.cmake
-set(path_rmm "${CMAKE_BINARY_DIR}/rapids-cmake/example-exports/build/cpm_rmm.cmake")
-set(to_match_string "${rmm_BINARY_DIR}")
+# verify that the expected path exists in also_fake_cpm_package.cmake
+set(path "${CMAKE_BINARY_DIR}/rapids-cmake/example-exports/build/cpm_CCCL.cmake")
+set(to_match_string "CMakeFiles/pkgRedirects")
 
-file(READ "${path_rmm}" contents)
+file(READ "${path}" contents)
 string(FIND "${contents}" "${to_match_string}" is_found)
-if(is_found EQUAL -1)
+if(NOT is_found EQUAL -1)
   message(FATAL_ERROR "rapids_cpm_cccl(BUILD_EXPORT_SET) failed to write out the proper possible_package_dir"
   )
 endif()
