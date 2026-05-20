371f1bbe06d31957d26ef5f95ac04371b74d1a59
Robert Maynard <rmaynard@nvidia.com>
2026-05-20 12:57:55 -0400

    add init_arch-multiple_project.cmake

---
+++ testing/cuda/init_arch-multiple_project.cmake
@@ -0,0 +1,30 @@
+# =============================================================================
+# cmake-format: off
+# SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION.
+# SPDX-License-Identifier: Apache-2.0
+# cmake-format: on
+# =============================================================================
+include(${rapids-cmake-dir}/cuda/init_architectures.cmake)
+
+set(CMAKE_CUDA_ARCHITECTURES "NATIVE")
+rapids_cuda_init_architectures()
+
+# Make sure we handle calling non cuda project first
+project(rapids-project LANGUAGES CXX)
+
+if(NOT CMAKE_CUDA_ARCHITECTURES STREQUAL "OFF")
+  message(FATAL_ERROR "CMAKE_CUDA_ARCHITECTURES=${CMAKE_CUDA_ARCHITECTURES} shouldn't be parsed yet"
+  )
+endif()
+project(rapids-project LANGUAGES CXX CUDA)
+
+if(NOT DEFINED CMAKE_CUDA_ARCHITECTURES)
+  message(FATAL_ERROR "CMAKE_CUDA_ARCHITECTURES should exist after calling rapids_cuda_init_architectures()"
+  )
+endif()
+
+if(CMAKE_CUDA_ARCHITECTURES STREQUAL "NATIVE")
+  message(FATAL_ERROR "rapids_cuda_init_architectures didn't init CUDA_ARCHITECTURES")
+endif()
+
+include("${rapids-cmake-testing-dir}/cuda/validate-cuda-native.cmake")
