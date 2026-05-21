f2e44ef021930342c125a7a9d3a7eb82c94cc129
Chuck Atkins <catkins@nvidia.com>
2026-05-21 10:44:47 -0400

    find: Support a third pre-fphsa custom code block in generated modules
    The existing INITIAL_CODE_BLOCK and FINAL_CODE_BLOCK arguments are
    populated before the any find_path or find_library calls and after
    target creation respectively. This introduces a new PRE_FPHSA_CODE_BLOCK
    argument that gets populated after the find_* calls but before
    find_package_handle_standard_args. The new argument is particulary useful
    for providing things like custom code to parse version information from
    header files for find_package_handle_standard_args to consider.

--- rapids-cmake/find/generate_module.cmake
+++ rapids-cmake/find/generate_module.cmake
@@ -26,6 +26,7 @@ Generate a Find*.cmake module for the requested package
                   [VERSION <version>]
                   [NO_CONFIG]
                   [INITIAL_CODE_BLOCK <code_block_variable>]
+                  [PRE_FPHSA_CODE_BLOCK <code_block_variable>]
                   [FINAL_CODE_BLOCK <code_block_variable>]
                   [BUILD_EXPORT_SET <name>]
                   [INSTALL_EXPORT_SET <name>]
@@ -87,6 +88,15 @@ when installed.
   Note: This requires the code block variable instead of the contents
   so that we can properly insert CMake code
 
+``PRE_FPHSA_CODE_BLOCK``
+  Optional value of the variable that holds a string of code that will
+  be executed after :cmake:command:`find_path` and :cmake:command:`find_library` calls
+  but before :cmake:command:`find_package_handle_standard_args`. This is particularly
+  useful for inserting code to extract version information from header files.
+
+  Note: This requires the code block variable instead of the contents
+  so that we can properly insert CMake code
+
 ``FINAL_CODE_BLOCK``
   Optional value of the variable that holds a string of code that will
   be executed as the last step of this config file.
@@ -137,7 +147,7 @@ function(rapids_find_generate_module name)
   list(APPEND CMAKE_MESSAGE_CONTEXT "rapids.find.generate_module")
 
   set(options NO_CONFIG)
-  set(one_value VERSION BUILD_EXPORT_SET INSTALL_EXPORT_SET INITIAL_CODE_BLOCK FINAL_CODE_BLOCK)
+  set(one_value VERSION BUILD_EXPORT_SET INSTALL_EXPORT_SET INITIAL_CODE_BLOCK PRE_FPHSA_CODE_BLOCK FINAL_CODE_BLOCK)
   set(multi_value HEADER_NAMES LIBRARY_NAMES INCLUDE_SUFFIXES)
   cmake_parse_arguments(_RAPIDS "${options}" "${one_value}" "${multi_value}" ${ARGN})
 
@@ -182,6 +192,14 @@ function(rapids_find_generate_module name)
       )
     endif()
     set(_RAPIDS_FIND_INITIAL_CODE_BLOCK "${${_RAPIDS_INITIAL_CODE_BLOCK}}")
+  endif()
+
+  if(DEFINED _RAPIDS_PRE_FPHSA_CODE_BLOCK)
+    if(NOT DEFINED ${_RAPIDS_PRE_FPHSA_CODE_BLOCK})
+      message(FATAL_ERROR "PRE_FPHSA_CODE_BLOCK variable `${_RAPIDS_PRE_FPHSA_CODE_BLOCK}` doesn't exist"
+      )
+    endif()
+    set(_RAPIDS_FIND_PRE_FPHSA_CODE_BLOCK "${${_RAPIDS_PRE_FPHSA_CODE_BLOCK}}")
   endif()
 
   if(DEFINED _RAPIDS_FINAL_CODE_BLOCK)
---
+++ testing/find/find_generate_module-back-pre-fphsa-code-block-var.cmake
@@ -0,0 +1,12 @@
+# =============================================================================
+# cmake-format: off
+# SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION.
+# SPDX-License-Identifier: Apache-2.0
+# cmake-format: on
+# =============================================================================
+include(${rapids-cmake-dir}/find/generate_module.cmake)
+
+rapids_find_generate_module(RapidsTest
+                            HEADER_NAMES rapids-cmake-test-header_only.hpp
+                            PRE_FPHSA_CODE_BLOCK var_doesn't_exist
+                            INSTALL_EXPORT_SET test_set)
