a773bfa14b0285b859d8938f83c7d058d1d1cf2d
Kyle Edwards <kyedwards@nvidia.com>
2026-05-22 11:42:25 -0400

    Fix how tests are executed (#1028)
    Before this change, it was possible for a test to "pass" without actually meeting the required conditions. If a `SHOULD_FAIL` argument was passed with a regular expression, and the test was supposed to have stdout or stderr that matches the regular expression, but in reality it had an exit code of 1 without matching the regular expression, it would pass. Change test execution so that the conditions are checked more carefully. Use a test script to do these checks.
    
    Authors:
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/rapids-cmake/pull/1028

--- testing/cython-core/cython-core_create_modules_errors.cmake
+++ testing/cython-core/cython-core_create_modules_errors.cmake
@@ -1,10 +1,10 @@
 # =============================================================================
 # cmake-format: off
-# SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 # cmake-format: on
 # =============================================================================
-include(${rapids-cmake-dir}/cython/create_modules.cmake)
+include(${rapids-cmake-dir}/cython-core/create_modules.cmake)
 
 # Test that a invocation without calling rapids_cython_init fails.
 rapids_cython_create_modules(SOURCE_FILES test.pyx)
