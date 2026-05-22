a13450e12e13b10bb6ca7ba70f23a9853cfbc03b
Kyle Edwards <kyedwards@nvidia.com>
2026-05-22 09:45:59 -0400

    Add tests for helper functions

---
+++ testing/json/array_extend.cmake
@@ -0,0 +1,26 @@
+# =============================================================================
+# cmake-format: off
+# SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION.
+# SPDX-License-Identifier: Apache-2.0
+# cmake-format: on
+# =============================================================================
+include(${rapids-cmake-dir}/json/array_extend.cmake)
+
+function(test_array_extend expected_result array values)
+  rapids_json_array_extend(actual_result "${array}" "${values}")
+  string(JSON equal EQUAL "${expected_result}" "${actual_result}")
+  if(NOT equal)
+    string(REPLACE "\n" "\n  " formatted_expected_result "${expected_result}")
+    string(REPLACE "\n" "\n  " formatted_actual_result "${actual_result}")
+    message(SEND_ERROR "Expected result:\n  ${formatted_expected_result}\nActual result:\n  ${formatted_actual_result}"
+    )
+  endif()
+endfunction()
+
+test_array_extend("[1]" "[]" "[1]")
+test_array_extend("[0, 1]" "[0]" "[1]")
+test_array_extend("[0, 1, 2]" "[0, 1]" "[2]")
+test_array_extend("[0, 1, 2, 3, 5]" "[0, 1]" "[2, 3, 5]")
+test_array_extend([==[[0, "str"]]==] "[0]" [==[["str"]]==])
+test_array_extend("[0, true]" "[0]" "[true]")
+test_array_extend("[0, null]" "[0]" "[null]")
