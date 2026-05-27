ee708faea65399b55124d3c6a7ddc98c8e5b967f
Arha Gatram <agatram@nvidia.com>
2026-05-27 14:54:30 -0700

    reverted back to lowercase install name

--- testing/cmake/default_install_component-sibling-mixed/check_installed_components.cmake
+++ testing/cmake/default_install_component-sibling-mixed/check_installed_components.cmake
@@ -7,7 +7,7 @@
 
 set(default_dependency_file "${install_dir}/default_dependency/dependencies/default_dependency.txt")
 set(specified_dependency_file "${install_dir}/testing/dependencies/specified_dependency.txt")
-set(project_name_unconfigured_file
+set(unconfigured_dependency_file
     "${install_dir}/unconfigured_dependency/dependencies/unconfigured_dependency.txt")
 
 if(NOT EXISTS "${default_dependency_file}")
@@ -16,7 +16,7 @@ endif()
 if(NOT EXISTS "${specified_dependency_file}")
   message(FATAL_ERROR "SpecifiedDependency did not install as component testing")
 endif()
-if(EXISTS "${project_name_unconfigured_file}")
-  message(FATAL_ERROR "UnconfiguredDependency incorrectly installed as component UnconfiguredDependency"
+if(EXISTS "${unconfigured_dependency_file}")
+  message(FATAL_ERROR "UnconfiguredDependency incorrectly installed as component unconfigureddependency"
   )
 endif()
