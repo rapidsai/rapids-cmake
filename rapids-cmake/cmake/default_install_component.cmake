7c7e32f40255f128a6812aa63c04d00846d40d3a
Arha Gatram <agatram@nvidia.com>
2026-05-28 10:57:46 -0700

    separated hooks to resolve sibling-mixed test

--- rapids-cmake/cmake/default_install_component.cmake
+++ rapids-cmake/cmake/default_install_component.cmake
@@ -42,20 +42,20 @@ function(rapids_cmake_default_install_component)
   cmake_parse_arguments(_RAPIDS "${options}" "${one_value}" "${multi_value}" ${ARGN})
 
   # Need to make sure we only install our project hook once
-  if(NOT "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/detail/default_install_component_hook.cmake" IN_LIST
-     CMAKE_PROJECT_INCLUDE)
-    list(APPEND CMAKE_PROJECT_INCLUDE
-         "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/detail/default_install_component_hook.cmake")
-    set(CMAKE_PROJECT_INCLUDE "${CMAKE_PROJECT_INCLUDE}" PARENT_SCOPE)
-  endif()
-
   if(_RAPIDS_DEFAULT_USE_PROJECT_NAME)
-    set_property(GLOBAL PROPERTY rapids_cmake_default_install_component_names ON)
-  endif()
-  if(_RAPIDS_PROJECT AND _RAPIDS_INSTALL_COMPONENT_NAME)
+    set(hook_file
+        "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/detail/default_install_component_default_hook.cmake")
+  elseif(_RAPIDS_PROJECT AND _RAPIDS_INSTALL_COMPONENT_NAME)
+    set(hook_file
+        "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/detail/default_install_component_project_hook.cmake")
     # Should only be set if the property doesn't already exist
     string(TOLOWER ${_RAPIDS_PROJECT} project)
     set_property(GLOBAL PROPERTY rapids_cmake_install_component_${project}
                                  ${_RAPIDS_INSTALL_COMPONENT_NAME})
+  endif()
+
+  if(hook_file AND NOT "${hook_file}" IN_LIST CMAKE_PROJECT_INCLUDE)
+    list(APPEND CMAKE_PROJECT_INCLUDE "${hook_file}")
+    set(CMAKE_PROJECT_INCLUDE "${CMAKE_PROJECT_INCLUDE}" PARENT_SCOPE)
   endif()
 endfunction()
--- rapids-cmake/cmake/detail/default_install_component_project_hook.cmake
+++ rapids-cmake/cmake/detail/default_install_component_project_hook.cmake
@@ -7,12 +7,8 @@
 
 string(TOLOWER ${PROJECT_NAME} project)
 
-get_property(_rapids_install_default_name GLOBAL
-             PROPERTY rapids_cmake_default_install_component_names)
 get_property(_rapids_install_project_name GLOBAL PROPERTY rapids_cmake_install_component_${project})
 
 if(_rapids_install_project_name)
   set(CMAKE_INSTALL_DEFAULT_COMPONENT_NAME "${_rapids_install_project_name}")
-elseif(_rapids_install_default_name)
-  set(CMAKE_INSTALL_DEFAULT_COMPONENT_NAME "${project}")
 endif()
